6dd5aee7f8599001a8bf6c91de95facd8fdf4411
SW Tools <swtools0@gmail.com>
2021-12-16 19:47:47 -0700

    Update get-airport-timezone

--- src/scripts/get-airport-timezone.ts
+++ src/scripts/get-airport-timezone.ts
@@ -1,8 +1,9 @@
 import console from 'console';
+import process from 'process';
 import * as Timezone from '../lib/timezones';
 
 async function main() {
-  const headers = await Timezone.fetchAirportTimezone('DEN');
+  const headers = await Timezone.fetchAirportTimezone(process.argv[2]);
 
   console.log(headers);
 }
