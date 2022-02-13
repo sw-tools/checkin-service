2d40710e62c70d8467579d3473661bf8fd1d87ce
SW Tools <swtools0@gmail.com>
2022-02-13 01:18:59 -0700

    Parameterize accountId

--- src/scripts/tests/test-create-eventbridge-rule.ts
+++ src/scripts/tests/test-create-eventbridge-rule.ts
@@ -1,13 +1,18 @@
 import * as EventBridge from '@aws-sdk/client-eventbridge';
 import { fromIni } from '@aws-sdk/credential-provider-ini';
+import assert from 'assert';
 import console from 'console';
 import * as Luxon from 'luxon';
+import process from 'process';
 import { putRule, putTarget } from '../../lib/create-eventbridge-rule';
 import { generateCronExpressionUtc } from '../../lib/cron-utils';
 import { Reservation } from '../../lib/reservation';
 import * as Queue from '../../lib/scheduled-checkin-ready-queue';
 
 async function main() {
+  assert(process.argv.length === 3, 'Invalid parameters');
+  const accountId = process.argv[2];
+
   const ruleFireDateTime = Luxon.DateTime.now().plus({ minutes: 1 });
 
   const cronExpression = generateCronExpressionUtc(ruleFireDateTime.toJSDate());
@@ -40,7 +45,7 @@ async function main() {
     ruleName,
     targetId: `test-${ruleFireDateTime.toSeconds()}`,
     message,
-    targetArn: 'arn:aws:sqs:us-west-2:737977559054:prod-checkin-service-scheduled-checkin-ready'
+    targetArn: `arn:aws:sqs:us-west-2:${accountId}:prod-checkin-service-scheduled-checkin-ready`
   });
 }
 
