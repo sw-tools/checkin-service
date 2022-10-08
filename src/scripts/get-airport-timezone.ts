b546dffcb14a74bf17857fc3a9b1450f22db7e5e
SW Tools <swtools0@gmail.com>
2022-10-08 08:59:10 -0600

    Use a hardcoded airport-timezone map instead of openflights because openflights is returning errors
    Co-authored-by: Rezer <Rezurok@yahoo.com>

--- src/scripts/get-airport-timezone.ts
+++ src/scripts/get-airport-timezone.ts
@@ -1,9 +1,9 @@
 import console from 'console';
 import process from 'process';
-import * as Timezone from '../lib/timezones';
+import { AirportCode, getAirportTimezone } from '../lib/timezones';
 
 async function main() {
-  const headers = await Timezone.fetchAirportTimezone(process.argv[2]);
+  const headers = await getAirportTimezone(process.argv[2] as AirportCode);
 
   console.log(headers);
 }
